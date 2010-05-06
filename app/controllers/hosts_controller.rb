6bc98f51303f6be618b334c8a9869d18d31d2332
John Krueger <john_t_krueger@john-kruegers-macbook-pro.local>
2010-05-06 17:49:50 -0600

    auth fixes

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -46,11 +46,5 @@ class HostsController < ApplicationController
     redirect_to hosts_url
   end
   
-  protected
-  
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['USERNAME'] && password == ENV['PASSWORD']
-    end
-  end
+ 
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -42,11 +42,5 @@ class ReportsController < ApplicationController
     redirect_to hosts_url
   end
   
-  protected
-  
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['USERNAME'] && password == ENV['PASSWORD']
-    end
-  end
+
 end
