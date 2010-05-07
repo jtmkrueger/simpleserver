0b927c8c662ea9e7c453d09e3503c64a6a078c78
John Krueger <john_t_krueger@john-kruegers-macbook-pro.local>
2010-05-07 14:31:15 -0600

    email fix

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,7 +1,7 @@
 class Mailer < ActionMailer::Base
 
   def sites_down(body)
-    recipients "jtmkrueger@gmail.com"
+    recipients "#{ENV['RECIPIENT1']}, #{ENV['RECIPIENT2']}"
     from       "john@x-cr.com"
     subject    "SITE(S) FAIL"
 
