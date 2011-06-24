4494688b7cf487c3182f7344f11ea3453806e275
jtmkrueger <john@xcellentcreations.com>
2011-06-24 15:13:16 -0600

    it's retarded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   protected
 
    def authenticate
-     authenticate_or_request_with_http_basic do |username, password|
-       username == ENV['USERNAME'] && password == ENV['PASSWORD']
+     #authenticate_or_request_with_http_basic do |username, password|
+     #  username == ENV['USERNAME'] && password == ENV['PASSWORD']
      end
    end
 end
